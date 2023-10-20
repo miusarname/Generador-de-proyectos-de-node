a9b01788578ef6383c5e03017d097729c1570d69
miusarname <oscarm.alvarezg@gmail.com>
2023-10-20 15:54:25 -0500

    feat: :sparkles: Se hizo el inicio

--- starter.sh
+++ starter.sh
@@ -1,4 +1,7 @@
-echo "bienvenido"
+echo "   B  I  E  N  V  E  N  I  D  O  S"
+echo "   P  R  E  S  I  O  N  A  R    E  N  T  E  R    P  A  R  A    I  N  I  C  I  A  R    L  A    I  N  S  T  A  L  A  C  I  O  N"
+echo " "
+read -p ""
 npm init -y && npm i -D nodemon morgan dotenv && npm i cors express express-rate-limit express-routes-versioning express-session express-validator jose jsonwebtoken passport passport-http-bearer 
 echo "import express from "express";
 import session from "express-session";
